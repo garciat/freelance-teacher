a8a7ea95ddc05685b102f820b5cfdc426748af32
Gabriel Garcia <gabriel@garciat.com>
2026-09-17 10:29:10 +0200

    read auth secret from env

--- app/session.ts
+++ app/session.ts
@@ -6,8 +6,13 @@ const AuthSessionSchema = z.object({
   email: z.string(),
 });
 
+const secret = Deno.env.get("APP_SESSION_AUTH_SECRET");
+if (!secret) {
+  throw new TypeError("unset APP_SESSION_AUTH_SECRET env var");
+}
+
 export const AuthSession = new SessionItem({
-  secret: new TextEncoder().encode("my happy secret"),
+  secret: new TextEncoder().encode(secret),
   cookieName: "auth",
   ttl: Temporal.Duration.from({ days: 7 }),
   schema: AuthSessionSchema,
